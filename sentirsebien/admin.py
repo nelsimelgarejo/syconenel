f5a68b02c6b8a4da2e40c7594aa0fa06bf61ba01
nelsimelgarejo <67168783+nelsimelgarejo@users.noreply.github.com>
2021-11-05 18:24:40 -0500

    1

---
+++ sentirsebien/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from .models import Perfil, FichaSociodemografica, ItemsTopicos, ComponenteBienestar, RespuestasPuente, ResultadoPerfil, DataUNFV
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin 
+
+# Register your models here.
+admin.site.register(Perfil)
+admin.site.register(FichaSociodemografica)
+admin.site.register(ResultadoPerfil)
+admin.site.register(RespuestasPuente)
+
+
+class ItemsTopicosResource(resources.ModelResource):
+    class Meta:
+        model = ItemsTopicos
+
+class ItemsTopicosAdmin(ImportExportModelAdmin,admin.ModelAdmin): # new
+    search_fields = ['item']
+    list_display = ('topico','item','inverso','creado','actualizado','estado',)
+    resources_class = ItemsTopicosResource
+
+admin.site.register(ItemsTopicos, ItemsTopicosAdmin)
+
+class ComponenteBienestarResource(resources.ModelResource):
+    class Meta:
+        model = ComponenteBienestar
+
+class ComponenteBienestarAdmin(ImportExportModelAdmin,admin.ModelAdmin): # new
+    search_fields = ['item']
+    list_display = ('perfil','topico','completado','estado','creado','actualizado',)
+    resources_class = ComponenteBienestarResource
+
+admin.site.register(ComponenteBienestar, ComponenteBienestarAdmin)
+
+class DataUNFVResource(resources.ModelResource):
+    class Meta:
+        model = DataUNFV
+
+class DataUNFVAdmin(ImportExportModelAdmin,admin.ModelAdmin): # new
+    search_fields = ['nombre_completo', 'codigo_estudiante', 'dni']
+    list_display = ('facultad','escuela','codigo_estudiante','correo','dni','nombre_completo','creado','activado','estado',)
+    resources_class = DataUNFVResource
+
+admin.site.register(DataUNFV, DataUNFVAdmin)
+
